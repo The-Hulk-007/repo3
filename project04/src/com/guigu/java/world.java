042276fbb18d84344d74ac1459a95bf12ed0bc1a
TheHulk <TheHulk@qq.com>
2019-12-13 11:20:15 +0800

    jiangjava工程添加到版本库

---
+++ project04/src/com/guigu/java/world.java
@@ -0,0 +1,14 @@
+package com.guigu.java;
+
+import java.util.Date;
+
+/**
+ * @author day start
+ * @create ${date} - ${time}
+ */
+public class world {
+    public static void main(String[] args){
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
